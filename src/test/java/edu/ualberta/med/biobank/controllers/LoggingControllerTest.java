8277e94f7fb0dffc1262afa964fe9ced9f382031
Nelson Loyola <nloyola@gmail.com>
2023-11-28 10:37:23 +0100

    Added preliminary tests for logging REST API endpoint

---
+++ src/test/java/edu/ualberta/med/biobank/controllers/LoggingControllerTest.java
@@ -0,0 +1,59 @@
+package edu.ualberta.med.biobank.controllers;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import edu.ualberta.med.biobank.test.ControllerTest;
+import edu.ualberta.med.biobank.test.Factory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+class LoggingControllerTest extends ControllerTest {
+
+    private final Logger logger = LoggerFactory.getLogger(LoggingControllerTest.class);
+
+    private final String ENDPOINT_INDEX_URL = "/logs";
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Autowired
+    private MockMvc mvc;
+
+    private Factory factory;
+
+    @BeforeEach
+    public void setup() throws Exception {
+        this.factory = new Factory(em);
+    }
+
+    @Test
+    @WithMockUser
+    public void getWhenEmptyTableIsOkAndEmpty() throws Exception {
+        this.mvc.perform(get(ENDPOINT_INDEX_URL))
+            .andExpect(status().isOk())
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(jsonPath("$.content", hasSize(0)))
+            .andExpect(jsonPath("$.numberOfElements", is(0)))
+            .andExpect(jsonPath("$.totalElements", is(0)))
+            .andExpect(jsonPath("$.totalPages", is(0)));
+    }
+
+    @Test
+    public void getWhenPresentAndUnauthorized() throws Exception {
+        this.mvc.perform(get(ENDPOINT_INDEX_URL))
+            .andExpect(status().isUnauthorized())
+            .andDo(MockMvcResultHandlers.print());
+    }
+}
