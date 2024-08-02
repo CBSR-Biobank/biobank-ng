cbdec5c8dcadf9eea67d29f423179f08c1823bf3
Nelson Loyola <nloyola@gmail.com>
2024-08-02 12:29:42 +0200

    When adding a patient, show error if the patient already exists

--- frontend/src/pages/patients/patient-add.tsx
+++ frontend/src/pages/patients/patient-add.tsx
@@ -1,20 +1,20 @@
+import { ApiError } from '@app/api/api';
+import { BbButton } from '@app/components/bb-button';
 import { PatientBreadcrumbs } from '@app/components/breadcrumbs/patients-breadcrubms';
 import { CircularProgress } from '@app/components/circular-progress';
 import { LabelledInput } from '@app/components/forms/labelled-input';
 import { StudySelect } from '@app/components/forms/study-select';
+import { Alert } from '@app/components/ui/alert';
 import { usePatientAdd } from '@app/hooks/use-patient';
 import { useStudyNames } from '@app/hooks/use-study';
 import { Patient } from '@app/models/patient';
 import { usePatientStore } from '@app/store';
-
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { z } from 'zod';
-
-import { BbButton } from '@app/components/bb-button';
 import { AdminPage } from '../admin-page';
 
 const schema = z.object({
@@ -28,6 +28,7 @@ export function PatientAddPage() {
   const [searchParams, _setSearchParams] = useSearchParams({ pnumber: '' });
   const studyNamesQry = useStudyNames();
   const patientAddMutation = usePatientAdd();
+  const [error, setError] = useState<ApiError | undefined>(undefined);
 
   const {
     control,
@@ -55,8 +56,11 @@ export function PatientAddPage() {
         navigate(`/patients/${newPatient.pnumber}`);
         reset();
       },
-      onError: () => {
-        reset();
+      onError: (err: any) => {
+        if (err?.error) {
+          const apiError = err as ApiError;
+          setError(apiError);
+        }
       },
     });
   };
@@ -90,6 +94,13 @@ export function PatientAddPage() {
             </div>
           </div>
         </form>
+        {error && (
+          <div className="flex items-center justify-center pt-8">
+            <Alert variant="destructive" className="border-2 border-red-600 bg-red-200 p-2 text-red-600">
+              {error.error.message}
+            </Alert>
+          </div>
+        )}
       </AdminPage>
     </>
   );
