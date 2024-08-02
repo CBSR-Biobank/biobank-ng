3987fa2d7e8c102eb7256f78b0de53c565f8ae2b
Nelson Loyola <nloyola@gmail.com>
2024-08-02 10:55:35 +0200

    Fix for migration
    Drops the USER_API_KEY DB table if it already exists prior to running the migration.

--- src/main/java/edu/ualberta/med/biobank/db/migration/V1_2__UserApiKey.java
+++ src/main/java/edu/ualberta/med/biobank/db/migration/V1_2__UserApiKey.java
@@ -7,19 +7,21 @@ public class V1_2__UserApiKey extends BaseJavaMigration {
 
     @Override
     public void migrate(Context context) throws Exception {
-        var sql =
-            """
-              CREATE TABLE USER_API_KEY (
-                ID integer NOT NULL auto_increment,
-                USER_ID integer NOT NULL,
-                API_KEY varchar(40) NOT NULL,
-                PRIMARY KEY (ID),
-                UNIQUE KEY `API_KEY` (`API_KEY`),
-                CONSTRAINT `UK_7qf2tl5mch5s2hm6kv9xh7irb` FOREIGN KEY (`USER_ID`) REFERENCES `principal` (`ID`)
-                    ON DELETE CASCADE ON UPDATE NO ACTION
-              ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
-            """;
         try (var statement = context.getConnection().createStatement()) {
+            statement.execute("DROP TABLE IF EXISTS USER_API_KEY");
+
+            var sql =
+                """
+                  CREATE TABLE USER_API_KEY (
+                    ID integer NOT NULL auto_increment,
+                    USER_ID integer NOT NULL,
+                    API_KEY varchar(40) NOT NULL,
+                    PRIMARY KEY (ID),
+                    UNIQUE KEY `API_KEY` (`API_KEY`),
+                    CONSTRAINT `UK_7qf2tl5mch5s2hm6kv9xh7irb` FOREIGN KEY (`USER_ID`) REFERENCES `principal` (`ID`)
+                        ON DELETE CASCADE ON UPDATE NO ACTION
+                  ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+                """;
             statement.execute(sql);
         }
     }
